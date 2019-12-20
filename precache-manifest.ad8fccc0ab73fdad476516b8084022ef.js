aada9108a330df1b153819b20a43e4fe040d8fb3
Emilio Mendez <emiliomendezcipriani@gmail.com>
2019-12-20 15:35:29 -0300

    Updates

---
+++ precache-manifest.ad8fccc0ab73fdad476516b8084022ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af62fc0a4064e42236c14d025f610469",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c08ae28919c25affc9f",
+    "url": "/monsters-rolodex/static/css/main.b5693d4f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c08ae28919c25affc9f",
+    "url": "/monsters-rolodex/static/js/main.5a285190.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
